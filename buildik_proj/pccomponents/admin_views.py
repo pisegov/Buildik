5a0fcd959d432856f0e71783533d642e675b776a
Viacheslav Boev <slava.boev94@gmail.com>
2020-12-21 18:22:16 +0300

    fix swagger

--- buildik_proj/pccomponents/admin_views.py
+++ buildik_proj/pccomponents/admin_views.py
@@ -16,14 +16,16 @@ def prepare_spec_model(obj):
 def prepare_belonging_model(obj):
     return pcc.BELONGINGS[obj.kwargs.get('belonging')]
 
-def prepare_queryset(model):
+def prepare_queryset(obj, prepare_model):
     try:
+        model = prepare_model(obj)
         return model.objects.all()
     except:
         return None
 
-def prepare_serializer(model, serializer):
+def prepare_serializer(obj, prepare_model, serializer):
     try:
+        model = prepare_model(obj)
         serializer.Meta.model = model
         serializer.Meta.fields = pccs._get_item_fields(model)
         return serializer
@@ -37,10 +39,10 @@ class ItemList(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAdminUser]
 
     def get_queryset(self):
-        return prepare_queryset(prepare_item_model(self))
+        return prepare_queryset(self, prepare_item_model)
 
     def get_serializer_class(self):
-        return prepare_serializer(prepare_item_model(self), pccs.ItemSerializer)
+        return prepare_serializer(self, prepare_item_model, pccs.ItemSerializer)
     
     # def list(self, request, *args, **kwargs):
     #     try:
@@ -58,45 +60,45 @@ class ItemDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [permissions.IsAdminUser]
 
     def get_queryset(self):
-        return prepare_queryset(prepare_item_model(self))
+        return prepare_queryset(self, prepare_item_model)
 
     def get_serializer_class(self):
-        return prepare_serializer(prepare_item_model(self), pccs.ItemSerializer)
+        return prepare_serializer(self, prepare_item_model, pccs.ItemSerializer)
 
 
 class SpecificationList(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAdminUser]
 
     def get_queryset(self):
-        return prepare_queryset(prepare_spec_model(self))
+        return prepare_queryset(self, prepare_spec_model)
 
     def get_serializer_class(self):
-        return prepare_serializer(prepare_spec_model(self), pccs.SpecificationSerializer)
+        return prepare_serializer(self, prepare_spec_model, pccs.SpecificationSerializer)
 
 class SpecificationDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [permissions.IsAdminUser]
 
     def get_queryset(self):
-        return prepare_queryset(prepare_spec_model(self))
+        return prepare_queryset(self, prepare_spec_model)
 
     def get_serializer_class(self):
-        return prepare_serializer(prepare_spec_model(self), pccs.SpecificationSerializer)
+        return prepare_serializer(self, prepare_spec_model, pccs.SpecificationSerializer)
 
 
 class BelongingList(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAdminUser]
 
     def get_queryset(self):
-        return prepare_queryset(prepare_belonging_model(self))
+        return prepare_queryset(self, prepare_belonging_model)
 
     def get_serializer_class(self):
-        return prepare_serializer(prepare_belonging_model(self), pccs.BelongingSerializer)
+        return prepare_serializer(self, prepare_belonging_model, pccs.BelongingSerializer)
 
 class BelongingDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [permissions.IsAdminUser]
 
     def get_queryset(self):
-        return prepare_queryset(prepare_belonging_model(self))
+        return prepare_queryset(self, prepare_belonging_model)
 
     def get_serializer_class(self):
-        return prepare_serializer(prepare_belonging_model(self), pccs.BelongingSerializer)
+        return prepare_serializer(self, prepare_belonging_model, pccs.BelongingSerializer)
