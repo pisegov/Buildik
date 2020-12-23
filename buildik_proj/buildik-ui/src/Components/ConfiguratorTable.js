7917f93cf85447296ee94a6ad88826c92bc805d0
Mikhail Pisegov <pisegov.mikhail@gmail.com>
2020-12-23 21:59:02 +0300

    add setup saving

--- buildik_proj/buildik-ui/src/Components/ConfiguratorTable.js
+++ buildik_proj/buildik-ui/src/Components/ConfiguratorTable.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button } from 'react-bootstrap';
 import SelectionModal from './Modal/SelectionModal';
+import axios from 'axios';
+import { Modal } from 'react-bootstrap';
+import NameForm from './NameForm';
 
 const styles = {
   ul: {
@@ -10,18 +13,61 @@ const styles = {
   },
 };
 
-const configuratorCategory = ['cpu', 'motherboard', 'case', 'gpu', 'cpu_cooler', 'ram', 'storage', 'power_supply_unit'];
+const configuratorCategories = [
+  'cpu',
+  'motherboard',
+  'case',
+  'gpu',
+  'cpu_cooler',
+  'ram',
+  'storage',
+  'power_supply_unit',
+];
 
 function ConfiguratorTable(props) {
+  const [setup, setSetup] = useState({});
+  const [setupID, setSetupID] = useState(null);
+  const [initItemList, setInitItemList] = useState({});
+
+  const [showSave, setShowSave] = useState(false);
+  const handleClose = () => setShowSave(false);
+  const handleShow = () => setShowSave(true);
+
+  const setSetupWithID = setup => {
+    setSetup(setup);
+    setSetupID(setup.id);
+  };
+
   return (
     <>
       <ul style={styles.ul}>
-        {configuratorCategory.map(category => {
+        {configuratorCategories.map(category => {
           // return <SetupComponent category={category} />;
-          return <SelectionModal category={category} />;
+          return (
+            <SelectionModal
+              category={category}
+              setSetup={setSetupWithID}
+              setupID={setupID}
+              itemList={[initItemList, setInitItemList]}
+            />
+          );
         })}
       </ul>
-      <Button style={{ textAlign: 'right' }}>Save</Button>
+      <Button onClick={handleShow}>Save</Button>
+
+      <Modal show={showSave} onHide={handleClose} size="lg">
+        <Modal.Header closeButton>
+          <Modal.Title>Назовите сборку</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <NameForm setSetup={setSetupWithID} initList={[initItemList, setInitItemList]} />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
 }
---
+++ buildik_proj/buildik-ui/src/Components/NameForm.js
@@ -0,0 +1,69 @@
+import { useState } from 'react';
+import axios from 'axios';
+import Cookies from 'universal-cookie';
+
+export default function NameForm({ setSetup, initList }) {
+  const [name, setName] = useState('');
+
+  const [initItemList, setInitItemList] = initList;
+
+  const cookies = new Cookies();
+  const csrftoken = cookies.get('csrftoken');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    try {
+      axios({
+        method: 'POST',
+        url: 'http://127.0.0.1:8000/api/setups/',
+        headers: {
+          'X-CSRFToken': csrftoken,
+        },
+        data: { name },
+      }).then(res => {
+        console.log(res);
+        console.log(res.data);
+
+        Object.entries(initItemList).map(([key, value]) => {
+          console.log(`Setup id: ${res.data.id}`);
+          console.log(`Item: ${JSON.stringify(value)}`);
+
+          axios({
+            method: 'POST',
+            url: 'http://127.0.0.1:8000/api/setups/setup_items/',
+            headers: {
+              'X-CSRFToken': csrftoken,
+            },
+            data: {
+              setup: res.data.id,
+              item: value.itemID,
+              number: value.number,
+            },
+          }).then();
+        });
+
+        setInitItemList({});
+
+        axios({
+          method: 'GET',
+          url: `http://127.0.0.1:8000/api/setups/${res.data.id}`,
+        }).then(response => {
+          setSetup(response.data);
+        });
+      });
+    } catch (e) {
+      console.log(`Error: ${e}`);
+    }
+  };
+
+  return (
+    <form onSubmit={e => handleSubmit(e)}>
+      <label>
+        Имя:
+        <input type="text" value={name} onChange={e => setName(e.target.value)} />
+      </label>
+      <input type="submit" value="Отправить" />
+    </form>
+  );
+}
