4614ae75f177052cb01e760fef6d8d4100479526
Mikhail Pisegov <pisegov.mikhail@gmail.com>
2020-12-25 05:17:38 +0300

    add navbar

--- buildik_proj/buildik-ui/src/App.js
+++ buildik_proj/buildik-ui/src/App.js
@@ -2,11 +2,20 @@ import { useEffect, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './Components/Navigation/Navbar';
 import ConfiguratorTable from './Components/ConfiguratorTable';
+import Cookies from 'universal-cookie';
 
 function App() {
+  const cookies = new Cookies();
+
+  const [isAutorised, setIsAutorised] = useState(false);
+
+  useEffect(() => {
+    setIsAutorised(Boolean(cookies.get('sessionid')));
+  }, []);
+
   return (
     <div className="App">
-      <Navbar />
+      <Navbar isAutorised={isAutorised} />
       <div className="wrapper">
         <h1>Configurator</h1>
         <ConfiguratorTable />
