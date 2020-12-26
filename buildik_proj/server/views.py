48af5a4e45ae7d137bc45fb0c6bf9d3f11263e77
Mikhail Pisegov <pisegov.mikhail@gmail.com>
2020-12-26 03:41:07 +0300

    add route for setups page

--- buildik_proj/server/views.py
+++ buildik_proj/server/views.py
@@ -4,8 +4,14 @@ from django.shortcuts import render, redirect
 def login(request):
     return render(request, 'login.html')
 
+
 def home(request):
     return render(request, 'home.html')
 
+
 def index(request):
+    return render(request, 'index.html', {})
+
+
+def setups(request):
     return render(request, 'index.html', {})
