bfaae3b1043c83e971e3272bec98e41daefa6cc4
Mikhail Pisegov <pisegov.mikhail@gmail.com>
2020-12-26 03:31:09 +0300

    add router

--- buildik_proj/buildik-ui/src/App.js
+++ buildik_proj/buildik-ui/src/App.js
@@ -3,23 +3,35 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './Components/Navigation/Navbar';
 import ConfiguratorTable from './Components/ConfiguratorTable';
 import Cookies from 'universal-cookie';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import SetupsPage from './Components/SetupsPage';
+import Login from './Components/Login';
 
 function App() {
   const cookies = new Cookies();
+  const [sessionId, setSessionId] = useState(cookies.get('sessionid'));
 
-  const [isAutorised, setIsAutorised] = useState(false);
-
-  useEffect(() => {
-    setIsAutorised(Boolean(cookies.get('sessionid')));
-  }, []);
+  // useEffect(() => {
+  //   setSessionId();
+  //   console.log('From app.js - ' + sessionId);
+  // }, []);
 
   return (
     <div className="App">
-      <Navbar isAutorised={isAutorised} />
-      <div className="wrapper">
-        <h1>Configurator</h1>
-        <ConfiguratorTable />
-      </div>
+      <Router>
+        <Navbar sessionId={sessionId} />
+        <Switch>
+          <Route path="/setups/" exact={SetupsPage}>
+            <SetupsPage sessionId={sessionId} />
+          </Route>
+          <Route path="/" exact={ConfiguratorTable}>
+            <div className="wrapper">
+              <h1>Buildik</h1>
+              <ConfiguratorTable />
+            </div>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
